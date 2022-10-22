a4813adfbef4b820c77f2f1878772bb6b117870b
nso89 <114881729+nso89@users.noreply.github.com>
2022-10-22 12:17:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import enum
 from pathlib import Path
 import shutil
 from typing import List
@@ -9,10 +10,11 @@ def get_file_count(folder: Path) -> int:
             file_count += 1
     return file_count
 
-def get_files_with_index(folder: Path, current_number_of_files: int) -> tuple[int,Path]:
-    for index,source in enumerate(folder.iterdir(), start=current_number_of_files):
-        if source.is_file():
-            yield index,source
+def is_dir(item: Path) -> bool:
+    return item.is_dir()
+
+def is_file(item: Path) -> bool:
+    return item.is_file()
 
 def main():
 
@@ -22,23 +24,23 @@ def main():
     to_delete : List[Path] = []
 
     try:
-
-        for folder in (folder for folder in SOURCE_REPOSITORY.iterdir() if folder.is_dir()):
+        
+        for folder in filter(is_dir,SOURCE_REPOSITORY.iterdir()):
             destination_repository_child = DESTINATION_REPOSITORY.joinpath(folder.stem)
             destination_repository_child.mkdir(exist_ok=True)
             number_of_files = get_file_count(destination_repository_child) + 1
-        
-            for index,source in get_files_with_index(folder=folder,current_number_of_files=number_of_files):
+            
+            for index, source in enumerate(filter(is_file,folder.iterdir()), start=number_of_files):
                 destination = destination_repository_child.joinpath(f"{index}{source.suffix}")
                 print(f"Source: {source} Destination: {destination}")
                 shutil.move(src=source,dst=destination)
-
+            
             to_delete.append(folder)
-        
+
         for folder in to_delete:
             folder.rmdir()
 
-    except (FileNotFoundError) as e:
+    except (FileNotFoundError,OSError) as e:
         print(e)
 
 if __name__ == "__main__":
