17ee276b6f77c050b7e091460df7f5b64143eb06
nso89 <114881729+nso89@users.noreply.github.com>
2022-10-31 11:09:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import shutil
 from typing import List
 
 def get_file_count(folder: Path) -> int:
+    """
+    Using a for loop and Path.is_file(),
+    we count all the files in a folder,
+    and return it.
+
+    Args:
+    folder: Path - the folder we're 
+    looping over.
+
+    Return:
+    file_count: the number of files in 
+    the folder.
+    """
     file_count : int = 0
     for file in folder.iterdir():
         if file.is_file():
@@ -10,10 +23,37 @@ def get_file_count(folder: Path) -> int:
     return file_count
 
 def is_dir(item: Path) -> bool:
+    """
+    Determine if the item is
+    in fact a folder.
+
+    Args:
+    item: Path - The item we're
+                 are checking.
+
+    Returns:
+    bool - true if item is a 
+           folder, false if 
+           it's a file. 
+    """
     return item.is_dir()
 
 def is_file(item: Path) -> bool:
+    """
+    Determine if the item is
+    in fact a file.
+
+    Args:
+    item: Path - The item we're
+                 are checking.
+
+    Returns:
+    bool - true if item is a 
+           file, false if 
+           it's a folder.
+    """
     return item.is_file()
+    
 
 def main():
 
@@ -24,15 +64,20 @@ def main():
 
     try:
         
+        # Using filder and is_dir(), we get only the folders as it's being determined.
         for folder in filter(is_dir,SOURCE_REPOSITORY.iterdir()):
             destination_repository_child = DESTINATION_REPOSITORY.joinpath(folder.stem)
-            destination_repository_child.mkdir(exist_ok=True)
+            # Create the directory if it doesn't exist, if it does, no exception 
+            # will be raised.
+            destination_repository_child.mkdir(exist_ok = True)
+            # Pick up the count and add 1 to continue.
             number_of_files = get_file_count(destination_repository_child) + 1
             
-            for index, source in enumerate(filter(is_file,folder.iterdir()), start=number_of_files):
+             # Using filder and is_file(), we get only the files as it's being determined.
+            for index, source in enumerate(filter(is_file,folder.iterdir()), start = number_of_files):
                 destination = destination_repository_child.joinpath(f"{index}{source.suffix}")
                 print(f"Source: {source} Destination: {destination}")
-                shutil.move(src=source,dst=destination)
+                shutil.move(src = source, dst = destination)
             
             to_delete.append(folder)
 
