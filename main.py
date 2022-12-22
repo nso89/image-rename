3bdc40a0f2801e7f17f39d7ecb4318d1a025b6c4
nso89 <114881729+nso89@users.noreply.github.com>
2022-12-22 12:38:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,9 @@ def get_file_count(folder: Path) -> int:
     """
     Returns the number of files in a 
     folder.
-
     Args:
         folder: Path - the folder 
         we're looping over.
-
     Return:
         file_count: the number of files 
         in the folder.
@@ -27,54 +25,20 @@ def get_file_count(folder: Path) -> int:
     return file_count
 
 
-def is_dir(item: Path) -> bool:
-    """
-    Determine if the item is
-    in fact a folder.
-
-    Args:
-        item: Path - The item 
-        we're checking.
-
-    Returns:
-        bool - true if item is 
-        a folder, false if it's 
-        a file. 
-    """
-    return item.is_dir()
-
-
-def is_file(item: Path) -> bool:
-    """
-    Determine if the item is
-    in fact a file.
-
-    Args:
-        item: Path - The item 
-        we're checking.
-
-    Returns:
-        bool - true if item 
-        is a file, false if 
-        it's a folder.
-    """
-    return item.is_file()
-    
-
 def main():
 
     to_delete : List[Path] = []
 
     try:
         
-        for folder in filter(is_dir,SOURCE_REPOSITORY.iterdir()):
+        for folder in (folder for folder in SOURCE_REPOSITORY.iterdir() if folder.is_dir()):
             destination_repository_child = DESTINATION_REPOSITORY.joinpath(folder.stem)
             destination_repository_child.mkdir(exist_ok = True)
             number_of_files = get_file_count(destination_repository_child) + 1
             
             print(f"\nSource: {folder} Destination: {destination_repository_child}")
 
-            for index, source in enumerate(filter(is_file,folder.iterdir()), start = number_of_files):
+            for index, source in enumerate((file for file in folder.iterdir() if file.is_file()), start = number_of_files):
                 destination = destination_repository_child.joinpath(f"{index}{source.suffix}")
                 print(f"Renaming {source.name} to {destination.name}")
                 shutil.move(src = source, dst = destination)
